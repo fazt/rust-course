b3bc6d579c48a6975396cb1b8b6ab25cf5ed27b6
Fazt <fazt@faztweb.com>
2022-09-15 02:22:02 -0500

    removed unused folder

--- cargo/src/main.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-  println!("Hola Mundo!");
-}
--- cargo/target/debug/deps/primer_programa-a8949970a14f81c8.d
+++
@@ -1,5 +0,0 @@
-c:\Users\fazt\Desktop\rust-course\cargo\target\debug\deps\primer_programa-a8949970a14f81c8.rmeta: src\main.rs
-
-c:\Users\fazt\Desktop\rust-course\cargo\target\debug\deps\primer_programa-a8949970a14f81c8.d: src\main.rs
-
-src\main.rs:
